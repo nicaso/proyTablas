8c694c2f0d245dabc7edf40f155c3d5323a6aebe
Programacion <Programacion@10.0.2.15>
2013-04-05 12:30:51 +0200

    TablasDe2

---
+++ src/pruebaproy/PruebaProy.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaproy;
+
+
+public class PruebaProy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Tabla del 2
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(i + " * 2 = " + (i*2));        
+        }
+    }
+}
