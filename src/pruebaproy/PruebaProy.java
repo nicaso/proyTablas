4ace387c28401b5cae1d12e3918ec30e9668af32
Programacion <Programacion@10.0.2.15>
2013-04-05 12:38:18 +0200

    TablasDe2,3y4

--- src/pruebaproy/PruebaProy.java
+++ src/pruebaproy/PruebaProy.java
@@ -11,16 +11,24 @@ public class PruebaProy {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int x=2;
+        int y=3;
+        int z=4;
         //Tabla del 2
         for (int i=1;i<=10;i++)
         {
-            System.out.println(i + " * 2 = " + (i*2));        
+            System.out.println(i + " * 2 = " + (i*x));        
         }
         
          //Tabla del 3
         for (int i=1;i<=10;i++)
         {
-            System.out.println(i + " * 3 = " + (i*3));        
+            System.out.println(i + " * 3 = " + (i*y));        
+        }
+        //Tabla del 4
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(i + " * 3 = " + (i*z));        
         }
     }
 }
